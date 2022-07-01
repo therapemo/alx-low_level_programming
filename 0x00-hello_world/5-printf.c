feac1515f997249494f3b663cecbe62837be0102
George Rapemo <gsosper@gmail.com>
2022-06-30 20:12:34 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#inlude <studio.h>
+#include <studio.h>
 /**
  * main - Entry point
- * 
  * Return: Always 0 (success)
- */
+ *
+ */
 int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
