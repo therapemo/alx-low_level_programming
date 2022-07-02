8a11b16066996540e0a462eeaf5291518665fb3c
George Rapemo <gsosper@gmail.com>
2022-07-02 09:28:01 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ if (lastd > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 }
 else if (lastd == 0)
-(
+}
 printf("Last digit of %d is %d and is 0\n", n, lastd);
 }
 else if (lastd < 6 && lastd != 0)
