266b7ceafe6e81494c2204dc3a7a0124a5e119f7
George Rapemo <gsosper@gmail.com>
2022-07-05 09:05:24 -0700

    alphabet(void)

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - Make the alphabet
+ * Return: void
+ **/
+void print_alphabet(void)
+{
+car c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
