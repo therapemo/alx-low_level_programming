2e3f8203f2256a7187f82dadfb9e777057432c32
George Rapemo <gsosper@gmail.com>
2022-07-25 05:56:05 -0700

    static library

--- 0x09-static_libraries/101-print_number.c
+++ 0x09-static_libraries/101-print_number.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
-  * print_number - Prints any integer with putchar
-  * @n: Number to prints
+  * print_number - Prints an integer.
+  * @n: The integer to prints.
   *
-  * Return: Nothing
+  * Return: Nothing!
   */
 void print_number(int n)
 {
-	unsigned int x;
+	unsigned int k = n;
 
 	if (n < 0)
 	{
-		_putchar('-');
 		n *= -1;
+		k = n;
+		_putchar('-');
 	}
 
-	x = n;
+	k /= 10;
 
-	if (x / 10)
-		print_number(x / 10);
+	if (k != 0)
+		print_number(k);
 
-	_putchar(x % 10 + '0');
+	_putchar((unsigned int) n % 10 + '0');
+
 }
+
