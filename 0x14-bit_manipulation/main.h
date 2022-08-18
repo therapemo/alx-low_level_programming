aaaccf1a912bcb4a809dfd6377abc9eb124a20c1
George Rapemo <gsosper@gmail.com>
2022-08-18 07:40:36 -0700

    updated main header

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,10 +4,11 @@
 /*
  * File: main.h
  * Desc: Header file containing prototypes for all functions
- *       written in the 0x13-bit_manipulation directory.
+ *       written in the 0x14-bit_manipulation directory.
  */
 
 int _putchar(char c);
+int _strlen(const char *s);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
